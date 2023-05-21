5bfe0d396eeb954b9442774605a204a117897209
rahmat-19 <rahmatjr023@gmail.com>
2023-05-21 09:35:28 +0700

    menyambungkan ke api event

--- resources/js/components/Event/Index.jsx
+++ resources/js/components/Event/Index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Input, Select, Button, Modal, DatePicker, Space, notification } from 'antd';
+import { Input, Select, Button, Modal, DatePicker, Space, notification, Table, Pagination } from 'antd';
 import { SearchOutlined, PlusOutlined } from '@ant-design/icons';
 
 import { QueryClient, QueryClientProvider } from "react-query";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 import dayjs from 'dayjs'
 import { getCookie } from "cookies-next";
@@ -31,6 +31,7 @@ if (document.getElementById("event-app")) {
 
 export function InjectApp() {
     const [open, setOpen] = useState(false);
+    const [data, setData] = useState([])
     const [api, contextHolder] = notification.useNotification();
     const [confirmLoading, setConfirmLoading] = useState(false);
     const [formData, setFormData] = useState({
@@ -38,6 +39,46 @@ export function InjectApp() {
         deskripsi: "",
         dateEvent: ''
     })
+
+    const columns = [
+        {
+            title: 'Nama Event',
+            dataIndex: 'nama',
+            // render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Tanggal Event',
+            dataIndex: 'tanggal',
+            render: (text) => dayjs(text).format('YYYY-MM-DD')
+        },
+        {
+            title: 'Deskripsi',
+            dataIndex: 'deskripsi',
+        },
+    ];
+
+    useEffect(() => {
+        fatchData()
+    }, [])
+
+    const fatchData = async () => {
+        const datas = await axios.get('/api/event_user', {
+            headers: {
+                Authorization : getCookie('token')
+            }
+        })
+        setData(datas.data.data.map(item => ({ ...item, key: item.id })))
+        setTableParams({
+            ...tableParams,
+            pagination: {
+                ...tableParams.pagination,
+                total: datas.data.data.length ,
+              // 200 is mock data, you should read it from server
+              // total: data.totalCount,
+            },
+        });
+    }
+
 
     const handleClick = () => {
         setOpen(true)
@@ -68,78 +109,97 @@ export function InjectApp() {
     };
 
     const handleOk = () => {
-        try {
-            axios.post('/api/event_user/create', {
-                user_id: 1,
-                nama: formData.name,
-                tanggal: dayjs(formData.dateEvent.$d).format('YYYY-MM-DD HH:mm:ss'),
-                deskripsi: formData.deskripsi
-            }, {
-                headers: {
-                    Authorization : getCookie('token')
-                }
-            })
+        if (!formData.name &&
+            !formData.dateEvent &&
+            !formData.deskripsi
+            ) {
 
-            openNotification(true)
-            setOpen(false)
-            setFormData({
-                    name: '',
-                    dateEvent: '',
-                    deskripsi: ''
+                api.info({
+                    message: 'Form Belum Di Isi',
+                    description: 'Lengkapai Form Tersebut Sebelum Subbmit',
+                    placement: 'bottomRight',
+                    duration: 1.5
                 })
-        } catch (error) {
+        } else {
+            try {
+                axios.post('/api/event_user/create', {
+                    user_id: 1,
+                    nama: formData.name,
+                    tanggal: dayjs(formData.dateEvent.$d).format('YYYY-MM-DD HH:mm:ss'),
+                    deskripsi: formData.deskripsi
+                }, {
+                    headers: {
+                        Authorization : getCookie('token')
+                    }
+                })
 
-            openNotification(false)
-            // setOpen(false)
+                openNotification(true)
+                setOpen(false)
+                setFormData({
+                        name: '',
+                        dateEvent: '',
+                        deskripsi: ''
+                    })
+                fatchData()
+            } catch (error) {
+
+                openNotification(false)
+                // setOpen(false)
+            }
+
         }
     }
+    const [selectionType, setSelectionType] = useState('checkbox');
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+    const onSelectChange = (newSelectedRowKeys) => {
+        console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+    const [tableParams, setTableParams] = useState({
+        pagination: {
+            current: 1,
+            pageSize: 5,
+            position: ['bottomLeft'],
+            showSizeChanger: true
+        },
+    });
+    const handleTableChange = (pagination) => {
+        setTableParams({
+            pagination,
+        });
+
+    };
 
 
     return (
         <div className="container-fluid">
             {contextHolder}
+            <ModalUi open={open} handleOk={handleOk} handleCancel={handleCancel} formData={formData} setFormData={setFormData}/>
             <div className="row">
                 <div className="col">
                     <div className="card">
                         <div className="card-body">
-                        <Modal
-                            title='Create New Event'
-                            open={open}
-                            onCancel={handleCancel}
-                            onOk={handleOk}
-                            width={700}
 
-                        >
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col">
-                                        <label htmlFor="nameEvent" className="form-label">Nama Event</label>
-                                        <Input id="nameEvent" onChange={e => setFormData({...formData, name: e.target.value})} value={formData.name} placeholder="Nama Event" />;
-                                    </div>
-                                    <div className="col">
-                                        <label htmlFor="tanggal" className="form-label">Tanggal</label>
-                                        <DatePicker id="tanggal"
-                                            style={{
-                                                width:'100%'
-                                            }}
-                                            onChange={(date, dateString) => setFormData({...formData, dateEvent: date})} value={formData.dateEvent}
-                                        />
-                                    </div>
-                                    <div className="col">
-                                        <label htmlFor="deskripsi" className="form-label">Deskripsi</label>
-                                        <TextArea id="deskripsi" rows={4}
-                                            onChange={e => setFormData({...formData, deskripsi: e.target.value})} value={formData.deskripsi}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-
-                        </Modal>
                             <div className="d-flex justify-content-between">
                                 <div></div>
                                 <div><Button type="primary" icon={<PlusOutlined />} onClick={handleClick}>Add New Event</Button></div>
                             </div>
+                            <br />
+                            <br />
+
                             <div>
+                            <Table
+                                rowSelection={rowSelection}
+                                columns={columns}
+                                dataSource={data}
+                                pagination={tableParams.pagination}
+
+                                onChange={handleTableChange}
+                            />
 
                             </div>
                         </div>
--- resources/js/components/Event/ModalUi.jsx
+++ resources/js/components/Event/ModalUi.jsx
@@ -1,14 +1,39 @@
-import { Input, Select, Button, Modal } from 'antd';
-export default function ModalUi({open, setOpen, confirmLoading, setConfirmLoading}) {
+import { Input, Modal, DatePicker } from 'antd';
+const { TextArea } = Input;
+export default function ModalUi({open, handleOk, handleCancel, formData, setFormData}) {
     return (
         <Modal
-            title='Create New Event'
-            open={open}
-            onOk={setOpen(false)}
-            onCancel={setOpen(false)}
-        >
-            <p>Ini Text</p>
+                            title='Create New Event'
+                            open={open}
+                            onCancel={handleCancel}
+                            onOk={handleOk}
+                            width={700}
 
-        </Modal>
+                        >
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col">
+                                        <label htmlFor="nameEvent" className="form-label">Nama Event</label>
+                                        <Input id="nameEvent" onChange={e => setFormData({...formData, name: e.target.value})} value={formData.name} placeholder="Nama Event" />;
+                                    </div>
+                                    <div className="col">
+                                        <label htmlFor="tanggal" className="form-label">Tanggal</label>
+                                        <DatePicker id="tanggal"
+                                            style={{
+                                                width:'100%'
+                                            }}
+                                            onChange={(date, dateString) => setFormData({...formData, dateEvent: date})} value={formData.dateEvent}
+                                        />
+                                    </div>
+                                    <div className="col">
+                                        <label htmlFor="deskripsi" className="form-label">Deskripsi</label>
+                                        <TextArea id="deskripsi" rows={4}
+                                            onChange={e => setFormData({...formData, deskripsi: e.target.value})} value={formData.deskripsi}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </Modal>
     )
 }
